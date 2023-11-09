3a6b046fb9c28f56a18a168debbe6b6ff7824060
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-11-09 21:10:28 +0400

    Name fix

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c367ec0601a5e80488155d7ab5f83cc.js"
+  "/precache-manifest.781a51b77ac52e10ffb67e28dad6909d.js"
 );
 
 self.addEventListener('message', (event) => {
